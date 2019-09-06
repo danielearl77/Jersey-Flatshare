2ca5b2d761607c2f1dde5545fb42b65a28251e9e
Daniel Earl <23332196+danielearl77@users.noreply.github.com>
2019-09-06 11:43:07 +0100

    Update functions.php
    DB Login details removed.

--- php/functions.php
+++ php/functions.php
@@ -1,20 +1,5 @@
 <?php
-// --- Database Code Local --- //
-//define('HST','localhost');
-//define('USR','root');
-//define('PSW','root');
-//define('DBN','g1v7c_854nz');
-
-// --- Database Code Live --- //
-//define('HST','norwichcitynewscouk.fatcowmysql.com');
-//define('USR','testing');
-//define('PSW','zaqwsx');
-//define('DBN','g1v7c_854nz');
-
-define('HST','norwichcitynewscouk.fatcowmysql.com');
-define('USR','hjxa04_9t3jq8');
-define('PSW','RIsMGc8jBU90');
-define('DBN','g1v7c_854nz');
+// DB Login removed from here.
 
 $link = mysqli_connect(HST, USR, PSW) or $failMsg = "Could not connect to server.";
 mysqli_select_db($link, DBN) or $failMsg = "Could not connect to DB.";
